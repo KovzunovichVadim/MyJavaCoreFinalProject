04251da1b218e8e83e61efca0fb959c70315dd98
AkumetsuVadim <akumetsu1988@gmail.com>
2023-12-26 14:40:43 +0300

    Change Class "AccountNumber" now in method "getAccountNumber" transmitted string and return account number in Map

--- src/AccountNumber/AccountNumber.java
+++ src/AccountNumber/AccountNumber.java
@@ -1,14 +1,13 @@
 package AccountNumber;
-import File.OperationWithFile;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AccountNumber {
     private Map<String, Integer> accountNumbers = new HashMap<>();
 
-    public Map<String, Integer> getAccountNumber(){
+    public Map<String, Integer> getAccountNumber(String account){
         try {
-            String[] numbers = OperationWithFile.fileAccountNumberReading().replace(" ","").split("\\|");
+            String[] numbers = account.split("\\r*\\n");
             for (String number:numbers){
                 String[] keyValue = number.replace(" ","").split("\\|");
                 if (!accountNumbers.containsKey(keyValue[0])){
